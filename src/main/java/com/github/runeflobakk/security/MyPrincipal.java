e4a7cffccbd2e4141b50f7f933d8dc1ed1c0c7e4
Rune Flobakk <flobakk@gmail.com>
2011-10-17 00:07:15 +0200

    fikset manglende implementasjon i MyPrincipal

--- src/main/java/com/github/runeflobakk/security/MyPrincipal.java
+++ src/main/java/com/github/runeflobakk/security/MyPrincipal.java
@@ -7,12 +7,15 @@ import org.springframework.security.core.userdetails.User;
 
 public class MyPrincipal extends User {
 
-    public MyPrincipal(String username, String password, Collection<? extends GrantedAuthority> authorities) {
+    private final String securityLevel;
+
+    public MyPrincipal(String username, String password, String securityLevel, Collection<? extends GrantedAuthority> authorities) {
         super(username, password, true, true, true, true, authorities);
+        this.securityLevel = securityLevel;
     }
 
     public String getSecurityLevel() {
-        throw new UnsupportedOperationException();
+        return securityLevel;
     }
 
 }
